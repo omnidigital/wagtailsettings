a1bef5963e6e1b58cb16468138c0d13d13d37aa4
Liam Brenner <liam.brenner@gmail.com>
2015-04-14 14:13:33 +1000

    Added icon argument to register_setting

--- wagtailsettings/registry.py
+++ wagtailsettings/registry.py
@@ -13,12 +13,16 @@ class Registry(object):
     def __init__(self):
         self.models = []
 
-    def register(self, model, **kwargs):
+    def register(self, model, icon='cog', **kwargs):
         if model in self.models:
             return model
 
         self.models.append(model)
-
+        icon_classes = 'icon icon-' + icon
+        if 'classnames' in kwargs:
+            kwargs['classnames'] += ' ' + icon_classes
+        else:
+            kwargs['classnames'] = icon_classes 
         @hooks.register('register_settings_menu_item')
         def hook():
             return MenuItem(
